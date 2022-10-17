7a77d911d1ff4098695f0c652ea0ac6e4aa3e4ff
Hamza056 <97276848+Hamza056@users.noreply.github.com>
2022-10-17 17:56:45 +0500

    second-commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import './App.css';
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
+  const notify = () => toast("welcome to new React app!");
   return (
   <div>
     React boiler plate with redux & React router 
+    <br />
+    <br />
+    <button className='btn' onClick={notify}>toster Notify!</button>
+        <ToastContainer />
   </div>
   
    
