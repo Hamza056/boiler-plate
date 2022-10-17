8b88911b15f6887ae938b1ba3e3f81747d91ea98
Hamza056 <97276848+Hamza056@users.noreply.github.com>
2022-10-17 17:15:54 +0500

    first-commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+
+const App = () => {
+  return (
+  <div>
+    React boiler plate with redux & React router 
+  </div>
+  
+   
+  )
+}
+
+export default App
